f3eabef97ec131d104fc11983545bfdb99e512b6
piyush9910 <suripiyush4@gmail.com>
2024-04-15 20:03:10 +0530

    Two Way Binding for Players Added

---
+++ src/components/Player.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    console.log(event)
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  //   let buttonCaption = 'Edit';
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" onChange={handleChange} required value={playerName} />
+    );
+    // buttonCaption = 'Save'
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
